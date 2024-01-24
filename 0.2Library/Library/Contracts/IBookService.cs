b2e5e58bac4cad1677acc78ffb59df061fdb819b
Hristina Petkova <hristinaspetkova2002@gmail.com>
2024-01-24 21:58:04 +0200

    two projects
    starting project library

---
+++ 0.2Library/Library/Contracts/IBookService.cs
@@ -0,0 +1,6 @@
+﻿namespace Library.Contracts
+{
+    public class IBookService
+    {
+    }
+}
---
+++ 0.2Library/Library/Data/Models/Book.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Data.Models
+{
+    public class Book
+    {
+
+        [Comment("Primary key")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Title of the book")]
+        [Required]
+        [MaxLength(50)]
+        public string Title { get; set; } = null!;
+
+        [Comment("Author of the book")]
+        [Required]
+        [MaxLength(50)]
+        public string Author { get; set; } = null!;
+
+        [Comment("Description of the book")]
+        [Required]
+        [MaxLength(5000)]
+        public string Description { get; set; } = null!;
+
+        [Comment("Image URL of the book")]
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+
+        [Comment("Rating of the book")]
+        [Required]
+        public decimal Rating { get; set; }
+
+        [Comment("Category ID of the book")]
+        [Required]
+        public int CategoryId { get; set; }
+
+        [Comment("Category of the book")]
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; } = null!;
+
+        public  List<IdentityUserBook> UsersBooks { get; set; } = new List<IdentityUserBook>();
+
+
+    }
+}
